e446e2998aa4fddd31e2b5a164a2f38abf8eb250
Pranish Shrestha <pranishshrestha181@gmail.com>
2022-07-01 11:06:55 +0545

    hamburger added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./navbar.css";
 
 function Navbar() {
+const[hamburgerOpen,setHamburgerOpen] =useState(false);
+const toggleHamburger =()=>{
+  setHamburgerOpen(!hamburgerOpen)    
+} 
   return (
 
-    // <nav>
-    //   <div className="snap">
-    //     snap
-    //   </div>
-    //   <input id="menu-toggle" type="checkbox" />
-    //   <label class='menu-button-container' for="menu-toggle">
-    //     <div class='menu-button'></div>
-    //   </label>
-    //   <ul className="menu">
-    //     <div className="left">
-    //       <li><a href="#">Features <svg width="10" height="6" xmlns="http://www.w3.org/2000/svg"><path stroke="#686868" stroke-width="1.5" fill="none" d="m1 1 4 4 4-4" /></svg></a></li>
-    //       <li><a href="#">Company <svg width="10" height="6" xmlns="http://www.w3.org/2000/svg"><path stroke="#686868" stroke-width="1.5" fill="none" d="m1 1 4 4 4-4" /></svg></a></li>
-    //       <li><a href="#">About</a></li>
-    //     </div>
-    //     <div className="right">
-    //       <li><a href="#">Login </a></li>
-    //       <li className='register'><a href="#">Register</a></li>
-    //     </div>
-    //   </ul>
-
-    //   <hr />
-    // </nav>
-    <>        <header className="navbar-container">
+    <>    
+        <header className="navbar-container">
       {/* <div className="topnav"> */}
       <div className="nav">
         <h1>
           <a href="/#" className="logo-wrap">
             snap</a>
         </h1>
-        <button className="nav-toggle" arial-label="toggle navigation"  onClick={() => {
-          
-        }}>
-          <span className="hamburger"></span>
+        <button className="nav-toggle" arial-label="toggle navigation"  onClick={toggleHamburger}  style={{display : hamburgerOpen ? 'block' : 'none'}}>
+          <span className="hamburger" ></span>
         </button>
+        {/* <ul> */}
         <ul>
           <li>
             <a href="/#">Features
@@ -58,8 +40,9 @@ function Navbar() {
         </ul>
       </div>
       <span className="btn-wrap right">
+      {/* style={{display : hamburgerOpen ? 'block' : 'none'}} */}
         <a href="/#">Login</a>
-        <a href="/#" className="btn register">register</a>
+        <a href="/#" className="register">register</a>
       </span>
       {/* </div> */}
     </header>
